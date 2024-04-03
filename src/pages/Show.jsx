bab8a5a9b2985a6511681f77e60628e873a0aec6
RachelY <yang.yijing@outlook.com>
2024-04-03 00:39:31 -0400

    show page styling added

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -13,29 +13,28 @@ export default function Show(props) {
     <h2>{aBill.status ? "Paid" : "Not Paid"}</h2>*/}
             <div>
                 {/* Update form */}
-                <Form action={`/update/${id}/`} method="POST">
+                <Form className="update-bill-form" action={`/update/${id}/`} method="POST">
                 <label htmlFor="bill_name">
-                    rename the bill
+                    Rename the bill
                     <input type="text" name="bill_name" id="bill_name" defaultValue={aBill.bill_name}/>
                 </label>
                 <label htmlFor="due_date">
-                    update the due date
+                    Update the due date
                     <input type="date" name="due_date" id="due_date" defaultValue={aBill.due_date}/>
                 </label>
                 <label htmlFor="amount">
-                    update the bill amount
+                    Update the bill amount
                     <input type="number" step="0.01" name="amount" id="amount" defaultValue={aBill.amount}/>
                 </label>
                 <label htmlFor="status">
-                    bill status
-                    <h4>paid or not</h4><input type="checkbox" name="status" id="status" defaultChecked={aBill.status}/>
+                    <h4>Paid</h4><input type="checkbox" name="status" id="status" defaultChecked={aBill.status}/>
                 </label>
-                <button style={{"backgroundColor": "blue"}}>Update our Bill</button>
+                <button>Update</button>
                 </Form>
 
                 {/* Delete Form */}
-                <Form action={`/delete/${id}/`} method="post">
-                    <button style={{"backgroundColor": "red"}}>Delete Bill</button>
+                <Form action={`/delete/${id}/`} method="post" className="Delete-bill">
+                    <button>Delete</button>
                 </Form>
             </div>
         </div>
